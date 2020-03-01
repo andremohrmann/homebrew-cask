84e7010653ff64588e4b2a8982949628ddc66e09
André Mohrmann <andremohrmann@users.noreply.github.com>
2020-03-01 01:18:38 +0100

    Initial commit

---
+++ brave-browser.rb
@@ -0,0 +1,20 @@
+cask 'brave-browser' do
+  version '80.1.4.95,104.95'
+  sha256 '954bb1f60d46a1f821f221d51bc7429ab07b8c4a0d3acf3c5e3391985a87116d'
+
+  # updates-cdn.bravesoftware.com/sparkle/Brave-Browser was verified as official when first introduced to the cask
+  url "https://brave-browser-downloads.s3.brave.com/latest/Brave-Browser.dmg"
+  appcast 'https://updates.bravesoftware.com/sparkle/Brave-Browser/stable/appcast.xml'
+  name 'Brave'
+  homepage 'https://brave.com/ioe757'
+
+  auto_updates true
+
+  app 'Brave Browser.app'
+
+  zap trash: [
+               '~/Library/Application Support/BraveSoftware/Brave-Browser',
+               '~/Library/Preferences/com.brave.Browser.plist',
+               '~/Library/Saved Application State/com.brave.Browser.savedState',
+             ]
+end
