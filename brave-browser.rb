ab63d87a3a1b03da5041f7b92fecc01709692798
André Mohrmann <andremohrmann@users.noreply.github.com>
2020-03-13 14:59:06 +0100

    Remove sha check

--- brave-browser.rb
+++ brave-browser.rb
@@ -1,6 +1,6 @@
 cask 'brave-browser' do
   version '80.1.4.95,104.95'
-  sha256 'd4493d1be958f74cf8c7dde59b56495bec4b36cf88a0b3504292ea7fcd92d4a3'
+#  sha256 'd4493d1be958f74cf8c7dde59b56495bec4b36cf88a0b3504292ea7fcd92d4a3'
 
   # updates-cdn.bravesoftware.com/sparkle/Brave-Browser was verified as official when first introduced to the cask
   url "https://brave-browser-downloads.s3.brave.com/latest/Brave-Browser.dmg"
